d6785dfc800d24bd1905c775bbe449004c47cb9f
penguinsource <mihaioprescu5@yahoo.com>
2013-03-21 16:32:12 -0600

    lil'

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+
+<head>
+<!--
+
+-->
+</head>
+
+<body>
+<p> Hello HTML </p>
+<?php echo "Hello PHP !"; ?>
+<?php header( 'Location: /other/blah.php' ) ; ?>
+</body>
+
+</html>
---
+++ other/productPage.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Hello from Blah !";
+// this should be the page that displays products (of any category)
+
+if (isset($_GET["productsType"])){
+	// display ..
+	// call the products of type 'productsType' from database
+	
+}
+
+
+?>
