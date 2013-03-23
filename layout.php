6cd1b5edcd075430bef837a1867298452f69f456
mbessett <mbessett@ualberta.ca>
2013-03-22 19:51:30 -0600

    layout shtuff

---
+++ layout.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+  
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <link href="design.css" rel="Stylesheet" type="text/css">
+</head>
+
+<body>
+  <div id="header">
+    <h1>top shit</h1>
+  </div>
+  <div id="page">
+    <div id="nav">
+      <p>some random nav bar</p>
+    </div>
+
+    <div id="content">
+      <div id="store">
+	<p>middle content type shit</p>
+      </div>
+
+      <div id="userbar">
+	<p>user info bar</p>
+      </div>
+    </div>
+    
+    <div id="footer">
+      <p>this is footer content</p>
+    </div>
+  </div>
+</body>
+
+</html>
