34135a1c431c4d50f3b552946f0e30faa45b785c
lorencs <mikuslorence@gmail.com>
2013-03-29 19:07:58 -0600

    done fancy category menu with shadows

--- functionsPHP/shopFuncs.php
+++ functionsPHP/shopFuncs.php
@@ -4,16 +4,27 @@ function printCategories($con, $selected){
     $query =   "SELECT * FROM category";								
 	$result = mysqli_query($con, $query) or die(" Query failed ");
     
-    $number = mysqli_num_rows($sql);
-    $i = 1;
+    $number = mysqli_num_rows($result);
+    $selIndex = 1;
     
+    while($row = mysqli_fetch_array($result)) {
+        $name = $row['name'];
+		if ($name == $selected) break;
+        $selIndex++;
+    }
+    
+    
+    mysqli_data_seek($result, 0);
+    $i = 1;
     while($row = mysqli_fetch_array($result)) {
 		$name = $row['name'];
         $class1 = ($name == $selected) ? "selmenuitem" : "menuitem";
         $class2 = ($i == $number) ? " menuitembottom" : "";
         $class = $class1 . $class2;
-        
-        echo "<a href='shop.php?category=$name'> <div class ='$class'>$name</div></a>\n";
+        $imgpre = ($i == ($selIndex - 1)) ? "<img style='display: ;position:relative; float:right;' src='../design/corner-br.png'>" : "";
+        $imgpost = ($i == ($selIndex + 1)) ? "<img style='display: ;position:relative; float:right;' src='../design/corner-tr.png'>" : "";
+        echo "";
+        echo "<a href='shop.php?category=$name'> <div class ='$class'>$name $imgpre $imgpost</div></a>\n";
         $i++;
     }
 }
