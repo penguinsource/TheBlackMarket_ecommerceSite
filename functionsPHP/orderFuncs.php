91023b58ca7af0bec5241f0a2c622686bf27a260
Mikus Lorencs <mikuslorence@gmail.com>
2013-04-07 01:03:56 -0600

    order page generates now
    - need to implement order function

--- functionsPHP/orderFuncs.php
+++ functionsPHP/orderFuncs.php
@@ -1,6 +1,6 @@
 <?php
 
-function checkAccess(){
+function printPage(){
 	//redirect and die if user not signed in
 	if(!isset($_SESSION['email'])){
 		echo "Please log in.";
@@ -47,6 +47,61 @@ function checkAccess(){
 		header('Location: cart');
 		die();
 	}
+	
+	
+	// PRINT ACTUAL PAGE IF ACCESS NOT RESTRICTED
+	
+	echo "<div id='header'> Confirm Order Details</div>\n";
+				
+	echo "<div style='margin-left:30px; margin-right:30px;overflow:auto;'>\n";
+		echo "<div id='cart'>\n";
+			echo "<div id='sub-header'> <span style='padding-left:10px'> Order <a href='/cart'> (edit) </a></span></div>\n";
+			
+			$cartJSON = json_decode($_SESSION['cart'], true);
+			
+			foreach($cartJSON['products'] as $item){
+				echo "<div class='item'>\n";
+					echo "<span style='display:inline-block;width:65%;'>" . $item['name'] . "</span>\n";
+					echo "<span style='display:inline-block;width:7%;text-align: right;'>x" . $item['quantity'] . "</span>\n";
+					echo "<span style='display:inline-block;width:26%;text-align: center;'>$" . ($item['price'] * $item['quantity']) . "</span>\n";
+				echo "</div>\n";
+			}
+						
+			echo "<div id='total-price'> $" . $cartJSON['total'] . " </div><div id='total-text'> Total: </div> \n";
+		echo "</div>\n";
+		
+		echo "<div id='address'>\n";
+			echo "<div id='sub-header'> <span style='padding-left:10px'> Shipping Address <a href='/user_profile'> (edit) </a></span></div>\n";
+			
+			$query = "SELECT * FROM user WHERE email = '" . $_SESSION["email"] . "'";        
+			$result = mysqli_query($con, $query);
+			
+			if($row = mysqli_fetch_array($result)) {
+				$firstname = $row['firstname'];
+				$lastname = $row['lastname'];
+				if (isset($lastname) && $lastname != "") $firstname = $firstname . " " . $lastname;
+				
+				$city = $row['city'];
+				$postal = $row['postal'];
+				$address = $row['address'];
+				$phone = $row['phone'];
+			}
+			
+			echo "<div style='padding-top:10px;'>\n";
+				echo "<div class='address-line'>$firstname</div>\n";
+				
+				if (isset($phone) && $phone != "") echo "<div class='address-line'>$phone</div>\n";
+				
+				echo "<div class='address-line'>$address</div>\n";
+				echo "<div class='address-line'>$city</div>\n";
+				echo "<div class='address-line'>$postal</div>\n";
+			echo "</div>\n";
+		
+		echo "</div>\n";		
+		
+	echo "</div>\n";
+	
+	echo "<a href='javascript:void(0);'><div class='button'> Place Order </div></a>\n";
 }
 
 function createOrder($user, $productArray){
--- order.php
+++ order.php
@@ -36,51 +36,7 @@
 		<div style="position:relative;">
 			
 			<div class='body'>		
-				<?php checkAccess($con); ?>
-				
-				<div id='header'> Confirm Order Details</div>
-				
-				<div style='margin-left:30px; margin-right:30px;'>
-					<div id='cart'>
-						<div id='sub-header'> <span style='padding-left:10px'> Order <a href='/cart'> (edit) </a></span></div>
-						
-						<div class='item'>
-							<span style='display:inline-block;width:65%;'>Bosch 4.6 Cu. Ft. Self-Clean Smooth Top Convection Range</span>
-							<span style='display:inline-block;width:7%;text-align: right;'>x3</span>
-							<span style='display:inline-block;width:26%;text-align: center;'>$799.99</span>
-						</div>
-						
-						<div class='item'>
-							<span style='display:inline-block;width:65%;'>LG 6.3 Cu. Ft. Self-Clean Smooth Top Range</span>
-							<span style='display:inline-block;width:7%;text-align: right;'>x1</span>
-							<span style='display:inline-block;width:26%;text-align: center;'>$899.99</span>
-						</div>
-						
-						<div class='item bottom'>
-							<span style='display:inline-block;width:65%;'>Bosch 5.0 Cu. Ft. Self-Clean Gas Range</span>
-							<span style='display:inline-block;width:7%;text-align: right;'>x1</span>
-							<span style='display:inline-block;width:26%;text-align: center;'>$599.99</span>
-						</div>
-						
-						<div id='total-price'> $2455.99 </div><div id='total-text'> Total: </div> 
-					</div>
-					
-					<div id='address'>
-						<div id='sub-header'> <span style='padding-left:10px'> Shipping Address <a href='/user_profile'> (edit) </a></span></div>
-						
-						<div style='padding-top:10px;'>
-							<div class='address-line'>Guyname Namerson</div>
-							<div class='address-line'>7801234567</div>
-							<div class='address-line'>507 Whattafuck Street</div>
-							<div class='address-line'>Washington DC</div>
-							<div class='address-line'>A4A 4A4</div>
-						</div>
-					
-					</div>
-					
-					<a href='javascript:void(0);'><div class='button'> Place Order </div>
-				</div>
-				
+				<?php printPage($con); ?>
 				
 			</div>
 			
