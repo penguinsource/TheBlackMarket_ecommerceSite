dbcfb65653cd1843aa161728b436c6aaf1971410
Mikus Lorencs <mikuslorence@gmail.com>
2013-04-06 22:17:47 -0600

    sync

--- order.php
+++ order.php
@@ -13,7 +13,7 @@
 	<!--<style type="text/css">
 		body{font-family: Calibri, Candara, Segoe, "Segoe UI", Optima, Arial, sans-serif;}
 	</style>-->
-	<LINK REL=STYLESHEET HREF="<?= $GLOBALS['baseURL']; ?>design/cart.css" TYPE="text/CSS">
+	<LINK REL=STYLESHEET HREF="<?= $GLOBALS['baseURL']; ?>design/order.css" TYPE="text/CSS">
 	<LINK REL=STYLESHEET HREF="<?= $GLOBALS['baseURL']; ?>design/mainmenu.css" TYPE="text/CSS">
     <link href='http://fonts.googleapis.com/css?family=Noto+Sans|Tauri|Economica|Istok+Web|Monda|Merriweather+Sans|Share+Tech+Mono|Roboto+Condensed|Oxygen|Maven+Pro' rel='stylesheet' type='text/css'>
 	<base href="//blackmarket5.hostei.com" />
@@ -38,7 +38,35 @@
 			<div class='body'>		
 				<?php checkAccess($con); ?>
 				
-				STUFF THAT SHOULDNT GET PRINTED IF IT DIED
+				<div id='header'> Confirm Order Details</div>
+				
+				<div style='margin-left:30px; margin-right:30px;'>
+					<div id='cart'>
+						<div id='sub-header'> Order <a href='/cart'> (edit) </a></div>
+						
+						<div class='item'>
+							<span style='display:inline-block;width:68%;'>Bosch 4.6 Cu. Ft. Self-Clean Smooth Top Convection Range</span>
+							<span style='display:inline-block;width:15%;'>x3</span>
+							<span style='display:inline-block;width:15%;'>$799.99</span>
+						</div>
+						
+						<div class='item'>
+							<span style='display:inline-block;width:68%;'>LG 6.3 Cu. Ft. Self-Clean Smooth Top Range</span>
+							<span style='display:inline-block;width:15%;'>x1</span>
+							<span style='display:inline-block;width:15%;'>$899.99</span>
+						</div>
+						
+						<div class='item'>
+							<span style='display:inline-block;width:68%;'>Bosch 5.0 Cu. Ft. Self-Clean Gas Range</span>
+							<span style='display:inline-block;width:15%;'>x1</span>
+							<span style='display:inline-block;width:15%;'>$599.99</span>
+						</div>
+					</div>
+					
+					<div id='address'>
+						<div id='sub-header'> Shipping Address <a href='/user_profile'> (edit) </a></div>
+					</div>
+				</div>
 			</div>
 			
 		</div>
