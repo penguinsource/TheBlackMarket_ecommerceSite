de317805bf3cae1ff0ea776dff9b7d9441fdf7be
penguinsource <mihaioprescu5@yahoo.com>
2013-04-06 20:46:09 -0600

    including category in search query done

--- functionsPHP/searchService.php
+++ functionsPHP/searchService.php
@@ -43,26 +43,38 @@
 	
 	$allCategories = 1;
 	if (isset($_POST['categories'])){
-		//$blah = json_decode($_POST['categories']);
 		$categoriesArray = json_decode(stripslashes($_POST['categories']));
-		/*print_r(stripslashes($_POST['categories']));
-		if ($categoriesArray->{'dishwashers'} && $categoriesArray->{'dishwashers'}){
-			echo "dish: " . $categoriesArray->{'dishwashers'} . ", ";
-		}*/
-		//echo "freezers: " . $categoriesArray->{'freezers'} . ", ";
-		
+		// print_r(stripslashes($_POST['categories']));
+		$categoriesSelected = 0;
 		foreach ($categoriesArray as $key => $value) {
-			echo "val: $key -> $value \n";
-			if (!$value){$allCategories = 0;}
+			//echo "val: $key -> $value \n";
+			if (!$value){$allCategories = 0;}else{$categoriesSelected++;}
 		}
 	}
 	
 	$con = connectToDB();		// open db connection
 	$basicQuery = "SELECT * FROM product WHERE ";
 	
-	$basicQuery .= "price > '1000'";
+	// APPENDING CATEGORIES to query
+	if (!$allCategories){			// if not all categories are included in the query.. then go through each and see which is selected
+		foreach ($categoriesArray as $key => $value) {
+			if ($value){			// if category '$key' is selected
+				if ($categoriesSelected > 1){
+					$basicQuery .= "pcategory = '$key' OR ";	// append to Query the selected category
+					--$categoriesSelected;
+				}else {				// append selected category 
+					$basicQuery .= "pcategory = '$key' ";	// this is the last category selected, don't add 'OR' at the end of the query
+					--$categoriesSelected;
+				}
+			}
+		}
+	}	// else if all categories are included in the query, do not append anything to query
+	
+	//SELECT * FROM product WHERE pcategory = 'dishwashers' OR pcategory = 'freezers'
+	
+	//$basicQuery .= "price > '1000'";
 	//echo "QUERY: ".$basicQuery." :END";
-	//echo stringOfQuery($con, $basicQuery);
+	echo stringOfQuery($con, $basicQuery);
 	closeDBConnection($con);    // close the database connection
 	
 	/*
