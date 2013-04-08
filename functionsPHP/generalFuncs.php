00a910d8f3ca9024d0e74dfb1be07d5bf1601c38
penguinsource <mihaioprescu5@yahoo.com>
2013-04-08 02:32:39 -0600

    --ignore

--- functionsPHP/generalFuncs.php
+++ functionsPHP/generalFuncs.php
@@ -10,8 +10,8 @@
 	} else if ($localhostON == 1){
 		$baseURL = "http://localhost/TheBlackMarket_ecommerceSite/";	// localhost mihai
 	} else if ($localhostON == 2){
-		//$baseURL = "http://cs410-06.cs.ualberta.ca/";
-		$baseURL = "http://cs410.cs.ualberta.ca:41061";
+		$baseURL = "http://cs410-06.cs.ualberta.ca/";
+		//$baseURL = "http://cs410.cs.ualberta.ca:41061";
 		
 	}
     /*    
