3a5f53f7488220a88c167c78329b64bda4ac0ba4
penguinsource <mihaioprescu5@yahoo.com>
2013-04-08 02:42:06 -0600

    --ignore

--- functionsPHP/generalFuncs.php
+++ functionsPHP/generalFuncs.php
@@ -12,7 +12,7 @@
 	} else if ($localhostON == 2){
 		//$baseURL = "http://cs410-06.cs.ualberta.ca/";
 		//$baseURL = "http://cs410.cs.ualberta.ca:41061/";
-		$baseURL = "localhost/";
+		$baseURL = "";
 		
 	}
     /*    
