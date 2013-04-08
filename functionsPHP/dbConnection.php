d6013b4b44ebb3aa4e79a7feccfa02e7600e7484
penguinsource <mihaioprescu5@yahoo.com>
2013-04-08 02:05:35 -0600

    --ignore-- trying to get stuff on VM to work.

--- functionsPHP/dbConnection.php
+++ functionsPHP/dbConnection.php
@@ -27,9 +27,10 @@
 		$con = mysqli_connect($GLOBALS["host"], $GLOBALS["username"], $GLOBALS["password"], $GLOBALS["database_name"]);
         // Check connection
         if (mysqli_connect_errno($con)){
-			//echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+			die();
         }else{
-			//echo "Connection Successful !";
+			echo "Connection Successful !";
             return $con;
         }
     }
