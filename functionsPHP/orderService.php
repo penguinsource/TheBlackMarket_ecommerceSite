eace79f773f0cc410cfc4751029c4b9c31418784
Mikus Lorencs <mikuslorence@gmail.com>
2013-04-08 21:22:33 -0600

    test

--- functionsPHP/orderService.php
+++ functionsPHP/orderService.php
@@ -66,7 +66,7 @@
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		$data = curl_exec($ch);
-		curl_close($ch)
+		curl_close($ch);
 		
 		return json_decode($data, true);
 	}
