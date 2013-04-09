9691b8ff35186f8905222a7dc07b26242fa1b1d9
lorencs <mikuslorence@gmail.com>
2013-04-09 14:00:46 -0600

    commit

--- functionsPHP/confirmFuncs.php
+++ functionsPHP/confirmFuncs.php
@@ -1,6 +1,9 @@
 <?php
 
+include("ChromePhp.php");
+
 function printPage($con){
+    
 	// redirect if not logged in or no get params
 	if (!isset($_SESSION['email'])){
 		//header('Location: shop');
@@ -23,6 +26,11 @@ function printPage($con){
 		
 	$result = mysqli_query($con, $query);
 	
+    $sqluserid = null;
+    $sqlorderid = null;
+    $data = null;    
+    $sqlemail = null;
+    
 	//redirect if orderif doesnt exist in pending orders, or logged in user doesnt match user of the order
 	if($row = mysqli_fetch_array($result)) {	
 		$sqluserid = $row['userid'];
@@ -40,7 +48,7 @@ function printPage($con){
 	} else {
 		//header('Location: shop');
 		echo "<div id='header'> Invalid Order</div><br>\n";
-		echo "<p>This order either does not exist or has already been processed.<p>";
+		echo "<p style='margin-left:30px;'>This order either does not exist or has already been processed.<p>";
 		die();
 	}
 	
@@ -50,9 +58,9 @@ function printPage($con){
     $deliveryArray = array(date('Y-m-d', strtotime(date("Y-m-d") . ' + 1 day')));	
 	
 	//print table header
-	echo "<div id='header'> Transaction Successful!</div><br>\n";
-	echo "<p>Here is your order summary.<p>";
-	echo "<div style='margin-left:30px; margin-right:30px;overflow:auto;'>\n";
+	echo "<div id='header'> Transaction Successful!</div>\n";
+	echo "<p style='margin-left:30px;'>Here is your order summary:</p>";
+	echo "<div style='margin-left:30px; margin-right:30px;overflow:hidden;'>\n";
 	echo "<div id='cart'>\n";
 	echo "<div id='sub-header'> 
 			<span style='display:inline-block;padding-left:10px;width:50%'> Product Name </span>
@@ -64,21 +72,30 @@ function printPage($con){
 	foreach($data['products'] as $product){
 		$pid = $product['pid'];
 		$pquantity = 0;
+        $pname = getProductName($pid, $con);
 		
 		$counter = 0;
+        echo "<div style='border-bottom: 1px solid #ECECEC;'>";
+        
 		//process each source of the product
 		foreach($product['sources'] as $source){
-			echo "<div style='width:100%;'>";
+            ChromePhp::log("got here");
+			echo "<div class='item' style='width:100%;'>";
 			
 			$url = $source['url'];
 			$quantity = $source['quantity'];
 			$price = $source['price'];
 			$name = $source['name'];
-			
 			$pquantity += $quantity;
 			if ($counter == 0){
-				echo "<span style='display:inline-block;padding-left:10px;width:50%'> Product Name </span>";
-			}
+				echo "<span style='display:inline-block;padding-left:10px;width:50%'> $pname </span>";
+			} else {
+                echo "<span style='display:inline-block;padding-left:10px;width:50%'>  </span>";
+            }
+
+            echo "<span style='display:inline-block;width:23%'> $name </span>\n";
+			echo "<span style='display:inline-block;width:12%'> x$quantity </span>\n";
+			echo "<span style='display:inline-block;width:12%'> $$price </span>\n";
 			
 			//if store is black market, subtract quantity
 			if ($name == "The Black Market"){
@@ -88,11 +105,12 @@ function printPage($con){
 				$currentQuantity = $row['quantity'];
 				$newQuantity = $currentQuantity - $quantity;
 				
-				$query = "UPDATE product SET quantity = '$newQuantity' WHERE id = '$sqluserid'";
-				mysqli_query($con, $query);
+				$query = "UPDATE product SET quantity = '$newQuantity' WHERE pid = '$sqluserid'";
+				$result = mysqli_query($con, $query);
+                echo $result;
 			//else must order from other store(s)
 			} else {
-				$response = curlPost($url . "/products/" . $pid . "/order");
+				$response = curlPost($url . "/products/" . $pid . "/order", $quantity);
 				$storeDate = $response['delivey_date'];
 				$storeId = $response['order_id'];
 				
@@ -102,12 +120,15 @@ function printPage($con){
 				//log product source
 				$query = "INSERT INTO orderSources VALUES ('$sqlorderid', '$storeId, '$url', '$pid', '$quantity', '$name')";	
 				$result = mysqli_query($con, $query);
+                echo $result;
 			}
 
 			echo "</div>\n";
 			$counter++;
 		}
 		
+        echo "</div>\n";
+        
 		//log quantity in productOrders
 		$query = "INSERT INTO productOrders VALUES ('$sqlorderid', '$pid, '$quantity')";	
 		$result = mysqli_query($con, $query);
@@ -138,8 +159,13 @@ function getUserEmail($id, $con){
 
 
 // MAKE THIS FUNCTION ?????????????????????????????????????/////////
-function getProductName($if, $con){
-
+function getProductName($id, $con){
+    $query= "SELECT * from product WHERE pid='$id'";
+		
+	$result = mysqli_query($con, $query);
+	$row = mysqli_fetch_assoc($result);
+	
+	return $row['pname'];
 }
 
 function getMaxDate($array){
